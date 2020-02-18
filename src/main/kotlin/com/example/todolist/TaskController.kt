ae5185842781a045aa603cf844dccedc4b7f15f9
pvcresin <pvcresin0730@gmail.com>
2020-02-18 13:04:21 +0900

    Create task

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -2,7 +2,10 @@ package com.example.todolist
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -14,5 +17,21 @@ class TaskController(private val taskRepository: TaskRepository) {
         val tasks = taskRepository.findAll()
         model.addAttribute("tasks", tasks)
         return "tasks/index"
+    }
+
+    @GetMapping("new")
+    fun index(form: TaskCreateForm): String {
+        return "tasks/new"
+    }
+
+    @PostMapping("")
+    fun create(@Validated form : TaskCreateForm,
+               bindingResult: BindingResult): String {
+
+        if (bindingResult.hasErrors()) return "tasks/new"
+
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
     }
 }
